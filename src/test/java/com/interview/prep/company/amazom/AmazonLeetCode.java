026441599b298474a3f3f7ebcfafa85ceaf5ec2d
Prateek Mulye <prateekmulye@prateeks-mbp.lan>
2020-06-18 01:13:29 -0400

    added some problems

--- src/test/java/com/interview/prep/company/amazom/AmazonLeetCode.java
+++ src/test/java/com/interview/prep/company/amazom/AmazonLeetCode.java
@@ -3,9 +3,7 @@ package com.interview.prep.company.amazom;
 import com.interview.prep.company.InterviewPrepApplicationTests;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 public class AmazonLeetCode extends InterviewPrepApplicationTests {
 
@@ -124,6 +122,46 @@ public class AmazonLeetCode extends InterviewPrepApplicationTests {
         return sb.toString();
     }
 
+    // 3Sum
+    public List<List<Integer>> threeSum(int[] nums){
+        // First sort the array
+        Arrays.sort(nums);
+        List<List<Integer>> output = new LinkedList<>();
+        // loop through the array to stop 2 positions before the array length to find the pair
+        for(int i = 0; i < nums.length - 2; i++){
+            // first condition of this if is for the first pass when i == 0 and
+            // the other condition is to check for no duplicates. if found we just skip the duplicate
+            if(i == 0 || (i > 0 && nums[i] != nums[i - 1])){
+                // from here the program is similar to 2sum having two pointer low and high like a sliding window
+                // The low pointer is set one position ahead of current position of i
+                int low = i + 1;
+                // The high pointer always starts from the end.
+                int high = nums.length - 1;
+                // Sum is going to store the value of compliment of the current value of i - 0.
+                // This will give us the sum to find the two elements which add up to the sum.
+                int sum = 0 - nums[i];
+                while(low < high){
+                    if(nums[low] + nums[high] == sum){
+                        output.add(Arrays.asList(nums[i],nums[low],nums[high]));
+                        // Skipping any duplicate values from the low pointer as the array is sorted
+                        while(low < high && nums[low] == nums[low + 1]) low++;
+                        // Skipping any duplicate values from the high pointer as the array is sorted
+                        while (low < high && nums[high] == nums[high - 1]) high--;
+                        // Incrementing the low and decementing the high pointer to the non duplicate values
+                        low++;
+                        high--;
+                        // if addition of values at low and high is greater than sum then decrementing high pointer,
+                        // if addition of values at low and high is less than sum then incrementing the low
+                    }else if(nums[low] + nums[high] > sum)
+                        high--;
+                    else
+                        low++;
+                }
+            }
+        }
+        return output;
+    }
+
     @Test
     public void testMaxSubArray(){
         int intArr1[] = {-2,1,-3,4,-1,2,1,-5,4};
@@ -161,4 +199,12 @@ public class AmazonLeetCode extends InterviewPrepApplicationTests {
         int num = 2020;
         System.out.println("Input: " + num + ", Convert Integer to Roman: " + intToRoman(num));
     }
+
+    @Test
+    public void testThreeSum(){
+        int intArray3[] = {-1, 0, 1, 2, -1, -4};
+        System.out.println("Input: " + Arrays.toString(intArray3) +
+                ", unique triplets in the array which gives the sum of zero: " + threeSum(intArray3));
+    }
+
 }
